c44232719ae1a75026d107713647c6a66ede682a
Danylo Ternovoi <121759315+TernovoiD@users.noreply.github.com>
2023-03-22 10:44:01 +0200

    Initial Commit

---
+++ Wreckpointer/WreckpointerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WreckpointerApp.swift
+//  Wreckpointer
+//
+//  Created by Danylo Ternovoi on 22.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct WreckpointerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
