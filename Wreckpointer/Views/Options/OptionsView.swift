9369b8605efc47488aa79d575a4fc2da82e26bb2
Danylo Ternovoi <danyloternovoi@gmail.com>
2024-01-23 10:05:38 +0200

    add restore purcheses

--- Wreckpointer/Views/Options/OptionsView.swift
+++ Wreckpointer/Views/Options/OptionsView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import WidgetKit
+import StoreKit
 
 struct OptionsView: View {
     
@@ -16,23 +17,13 @@ struct OptionsView: View {
     @Binding var showModeratorPage: Bool
     var body: some View {
         NavigationView {
-            List {
-                VStack(alignment: .leading) {
-                    Toggle("Hide unapproved", isOn: $hideUnapprovedWrecks)
-                    Text("This option will hide all wrecks that moderators have not yet checked. All newly created and created by AI wrecks receive unapproved status.")
-                        .font(.caption2)
-                        .fontWeight(.light)
-                        .foregroundStyle(Color.secondary)
-                        .padding(.top, 5)
+            ScrollView {
+                VStack(spacing: 25) {
+                    hideUnapprovedSwitch
+                    moderatorModeSwitch
+                    resetPurchasesButton
                 }
-                VStack(alignment: .leading) {
-                    Toggle("Moderator mode", isOn: $showModeratorPage)
-                    Text("This option activates the moderator mode. Please note, that this mode requires authorisation. Contact wreckpointer@gmail.com to get a moderator account.")
-                        .font(.caption2)
-                        .fontWeight(.light)
-                        .foregroundStyle(Color.secondary)
-                        .padding(.top, 5)
-                }
+                .padding(.horizontal)
             }
             .navigationTitle("Options")
             .toolbar {
@@ -49,4 +40,48 @@ struct OptionsView: View {
 
 #Preview {
     OptionsView(showModeratorPage: .constant(false))
+}
+
+private extension OptionsView {
+    
+    var hideUnapprovedSwitch: some View {
+        VStack(alignment: .leading) {
+            Toggle("Hide unapproved", isOn: $hideUnapprovedWrecks)
+                .font(.headline)
+            Text("This option will hide all wrecks that moderators have not yet checked. All newly created and created by AI wrecks receive unapproved status.")
+                .font(.caption2)
+                .fontWeight(.light)
+                .foregroundStyle(Color.secondary)
+                .padding(.top, 5)
+        }
+    }
+    
+    var moderatorModeSwitch: some View {
+        VStack(alignment: .leading) {
+            Toggle("Moderator mode", isOn: $showModeratorPage)
+                .font(.headline)
+            Text("This option activates the moderator mode. Please note, that this mode requires authorisation. Contact wreckpointer@gmail.com to get a moderator account.")
+                .font(.caption2)
+                .fontWeight(.light)
+                .foregroundStyle(Color.secondary)
+                .padding(.top, 5)
+        }
+    }
+    
+    var resetPurchasesButton: some View {
+        Button(action: {
+            Task {
+                try? await AppStore.sync()
+            }
+        }, label: {
+            Color.clear
+                .frame(height: 50)
+                .overlay {
+                    Text("Restore Purchases")
+                        .font(.headline)
+                }
+                .coloredBorder(color: .accent)
+        })
+    }
+    
 }
